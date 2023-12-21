76b119df3b479396fa7cef61685a91ecb47b06b9
lgm1007 <flxl654@gmail.com>
2023-12-21 10:59:47 +0900

    feat: 미들웨어 - logger 미들웨어 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { LoggerMiddleware } from './logger/logger.middleware';
 
 @Module({
   imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+// 미들웨어를 포함하는 모듈은 NestModule 인터페이스를 구현해야 한다.
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes('*');
+  }
+}
