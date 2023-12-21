096b9c57632133a02e1bb4a0ba981b020c39e489
lgm1007 <flxl654@gmail.com>
2023-12-21 11:07:37 +0900

    feat: users 임의 라우터 생성, users 서비스 exports

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Patch, Post, Put } from "@nestjs/common";
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  getAllUsers() {
+    return 'get all users';
+  }
+
+  @Get(':id')
+  getOneUser(@Param() param) {
+    return `get ${param} user`;
+  }
+
+  @Post()
+  createUser() {
+    return 'create one user';
+  }
+
+  @Put(':id')
+  updateUser() {
+    return 'update one user';
+  }
+
+  @Patch(':id')
+  updatePartialUser() {
+    return;
+  }
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,6 +4,7 @@ import { UsersService } from './users.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
+  exports: [UsersService], // 다른 모듈에서 접근 가능하도록 public 해줌
 })
 export class UsersModule {}
