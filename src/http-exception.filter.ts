9b2fcd28ff83ddccfd623aff9db420965aed9331
lgm1007 <flxl654@gmail.com>
2023-12-21 17:13:14 +0900

    feat: exception filter를 error 타입에 따라 커스텀하기

--- src/http-exception.filter.ts
+++ src/http-exception.filter.ts
@@ -13,13 +13,23 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
-    const error = exception.getResponse();
+    const error = exception.getResponse() as
+      | string
+      | { error: string; statusCode: number; message: string | string[] };  // error는 문자열 또는 error, statusCode, message 로 구성된 객체일 수 있음
 
-    response.status(status).json({
-      statusCode: status,
-      timestamp: new Date().toISOString(),
-      path: request.url,
-      error: error,
-    });
+    if (typeof error === 'string') {
+      response.status(status).json({
+        success: false,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+        error,
+      });
+    } else {
+      response.status(status).json({
+        success: false,
+        timestamp: new Date().toISOString(),
+        ...error
+      });
+    }
   }
 }
